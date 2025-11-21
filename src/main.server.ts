dfb592b101992a6ecd143862f2e7792d7cb43909
JesusRodrigoV <jaicelvelasco105@gmail.com>
2025-11-21 09:43:53 -0400

    Actualizacion a angular v21

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,11 +1,19 @@
+import { provideZonelessChangeDetection } from "@angular/core";
 import {
   bootstrapApplication,
   BootstrapContext,
-} from '@angular/platform-browser';
-import { App } from './app/app';
-import { config } from './app/app.config.server';
+} from "@angular/platform-browser";
+import { App } from "./app/app";
+import { config } from "./app/app.config.server";
 
 const bootstrap = (context: BootstrapContext) =>
-  bootstrapApplication(App, config, context);
+  bootstrapApplication(
+    App,
+    {
+      ...config,
+      providers: [provideZonelessChangeDetection(), ...config.providers],
+    },
+    context,
+  );
 
 export default bootstrap;
